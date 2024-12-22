625223e0e43d175d3eb4c04ccec781bc54af9377
raza ayaz <razaayaz@yahoo.com>
2024-12-23 00:04:45 +0500

    Add files via upload

---
+++ abcd.js
@@ -0,0 +1,29 @@
+var username= "RAZA AYAZ"
+var message="HELLO WORLD"
+var student="ALI RAZA"
+
+var email = "habib@apha.org.pk"
+var sentence="Yah! i can write HTML content through Java script"
+var book= " A smarter way to learn JavaScript"
+
+console.log(student);
+console.log(username);
+console.log(message);
+console.log(sentence);
+console.log(book);
+
+
+
+
+
+alert (username); 
+alert(message);
+alert(student);
+ 
+alert("My email address is: " + email);
+alert(" I am trying to learn from a book" + book)
+
+alert(sentence);
+
+document.write(sentence)
+
---
+++ pizza.js
@@ -0,0 +1,18 @@
+var pizza = "pizza"
+console.log(pizza);
+alert(pizza);
+alert("pizza\npizz\npiz\npi\np")
+
+var logo="-"
+var mid="&"
+var arow="^"
+var round="@"
+console.log(logo);
+console.log(mid);
+console.log(arow);
+
+
+alert(logo + logo + logo +logo +logo +logo +logo +logo +mid +arow +round +arow +mid +logo + logo + logo +logo +logo +logo +logo +logo)
+document.write("---------&^@^&---------");
+
+
